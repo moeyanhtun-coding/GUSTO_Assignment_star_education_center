743a3c32914ca5ed056d57805195a920332badb4
moeyanhtun-coding <moeyan227970@gmail.com>
2024-09-28 23:53:12 +0630

    [Add] Custom widget

---
+++ lib/ulti.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget margin({
+  required double width,
+  required double height,
+}) {
+  return SizedBox(
+    width: width,
+    height: height,
+  );
+}
