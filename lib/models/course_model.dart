9d58b12c588cf1de41eb55a97bdc6175b4e37b81
Moe Yan Htun <moeyan227970@gmail.com>
2024-10-08 15:58:35 +0630

    -

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -1,9 +1,10 @@
 class CourseModel {
   final String _cId;
   String courseName;
+  String courseDuration;
   double fees;
 
-  CourseModel(this._cId, this.courseName, this.fees);
+  CourseModel(this._cId, this.courseName, this.fees, this.courseDuration);
 
   String get courseId => _cId;
 }
