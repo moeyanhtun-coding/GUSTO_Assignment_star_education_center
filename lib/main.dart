8ac75a111f8138338d6d967d157c6131890d4ce7
Moe Yan Htun <moeyan227970@gmail.com>
2024-10-18 09:09:43 +0630

    -

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,12 @@ import 'package:star_education_center/firebase_options.dart';
 import 'package:star_education_center/pages/home_page.dart';
 import 'package:star_education_center/pages/login_page.dart';
 import 'package:star_education_center/pages/register_page.dart';
+
+// Main function which is the entry point of the app
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with platform-specific options (Android, iOS, etc.)
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -14,12 +18,14 @@ void main() async {
   runApp(const MyApp());
 }
 
+// Define the routes using GetX's GetPage. Each route is associated with a page.
 final routes = [
   GetPage(name: '/login', page: () => LoginPage()),
   GetPage(name: '/register', page: () => RegisterPage()),
   GetPage(name: '/home', page: () => HomePage()),
 ];
 
+// MyApp widget is the root of your application
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
