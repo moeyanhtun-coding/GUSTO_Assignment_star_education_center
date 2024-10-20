0de66b9bf5e283cc94410fcb5f1091d8251099a7
moeyanhtun-coding <moeyan227970@gmail.com>
2024-10-20 17:00:39 +0630

    -

--- lib/pages/courses_page.dart
+++ lib/pages/courses_page.dart
@@ -6,20 +6,24 @@ import 'package:star_education_center/services/course_firestore_service.dart';
 import 'package:star_education_center/ulti.dart';
 import 'package:uuid/uuid.dart';
 
-// Initialize the dependencies
+// Initialize Firebase Firestore instance
 final FirebaseFirestore _db = FirebaseFirestore.instance;
+// Initialize UUID service for unique IDs
 final UuidService _uuidService =
     DefaultUuidService(); // Using the default implementation
+// Logger service for logging
 final LoggerService _loggerService = LoggerService();
+// Uuid instance for generating unique IDs
 final Uuid uuid = Uuid();
 
-// Create an instance of CourseFirestoreService with the required dependencies
+// Initialize CourseFirestoreService with required services (Firestore, UUID, Logger)
 final CourseFirestoreService _coursesService = CourseFirestoreService(
   db: _db,
   uuidService: _uuidService,
   loggerService: _loggerService,
 );
 
+// Stateful widget for displaying the Courses Page
 class CoursesPage extends StatefulWidget {
   const CoursesPage({super.key});
 
@@ -28,27 +32,29 @@ class CoursesPage extends StatefulWidget {
 }
 
 class _CoursesPageState extends State<CoursesPage> {
+  // Main widget for the courses page
   @override
   Widget build(BuildContext context) {
     return _coursesPage();
   }
 
+  // Method to build the layout of the courses page
   Widget _coursesPage() {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      color: const Color.fromARGB(255, 0, 17, 32),
+      color: const Color.fromARGB(255, 0, 17, 32), // Dark background color
       child: const SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(width: 0, height: 30),
-            Header1(),
-            SizedBox(width: 0, height: 20),
-            Header2(),
-            SizedBox(width: 0, height: 30),
-            SearchBar(),
-            SizedBox(width: 0, height: 20),
-            CourseList()
+            SizedBox(width: 0, height: 30), // Spacing
+            Header1(), // First header widget
+            SizedBox(width: 0, height: 20), // Spacing
+            Header2(), // Second header widget
+            SizedBox(width: 0, height: 30), // Spacing
+            SearchBar(), // Search bar widget
+            SizedBox(width: 0, height: 20), // Spacing
+            CourseList() // List of courses
           ],
         ),
       ),
@@ -56,6 +62,7 @@ class _CoursesPageState extends State<CoursesPage> {
   }
 }
 
+// Stateless widget for displaying the first header
 class Header1 extends StatelessWidget {
   const Header1({super.key});
 
@@ -63,6 +70,7 @@ class Header1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
+        // Title text for the Education Center
         Text(
           "Star Education Center's",
           style: TextStyle(
@@ -71,6 +79,7 @@ class Header1 extends StatelessWidget {
             fontSize: 25,
           ),
         ),
+        // Subtitle text for courses
         Text(
           "Courses",
           style: TextStyle(
@@ -84,6 +93,7 @@ class Header1 extends StatelessWidget {
   }
 }
 
+// Stateless widget for displaying the second header
 class Header2 extends StatelessWidget {
   const Header2({super.key});
 
@@ -91,14 +101,16 @@ class Header2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
+        // Informational text about students
         Text(
-          "Above 4,500 students are learning at Star Eductaion",
+          "Above 4,500 students are learning at Star Education",
           style: TextStyle(
             color: Colors.white,
           ),
         ),
+        // Call to action for the user
         Text(
-          "Center. Why are you waiting for ?",
+          "Center. Why are you waiting for?",
           style: TextStyle(
             color: Colors.white,
           ),
@@ -108,31 +120,33 @@ class Header2 extends StatelessWidget {
   }
 }
 
+// Stateless widget for the search bar
 class SearchBar extends StatelessWidget {
   const SearchBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25),
+      padding:
+          EdgeInsets.symmetric(horizontal: 25), // Padding around the search bar
       child: TextField(
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(color: Colors.white), // White text color for the input
         decoration: InputDecoration(
+          // Border styling for enabled state
           enabledBorder: OutlineInputBorder(
-            borderSide:
-                BorderSide(color: Colors.blue), // Change to your desired color
+            borderSide: BorderSide(color: Colors.blue), // Blue border
             borderRadius: BorderRadius.all(
               Radius.circular(25),
             ),
           ),
-          hintText: "What are you looking for ?",
+          hintText: "What are you looking for?", // Placeholder text
           hintStyle: TextStyle(
             color: Colors.grey,
             fontWeight: FontWeight.bold,
           ),
           prefixIcon: Icon(
             Icons.search,
-            color: Colors.blue,
+            color: Colors.blue, // Blue search icon
           ),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(
@@ -145,30 +159,34 @@ class SearchBar extends StatelessWidget {
   }
 }
 
+// Stateless widget for displaying the list of courses
 class CourseList extends StatelessWidget {
   const CourseList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // StreamBuilder to fetch courses from Firestore in real-time
     return StreamBuilder<QuerySnapshot>(
-      stream: _coursesService.getCourses(),
+      stream: _coursesService.getCourses(), // Fetch courses stream
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<DocumentSnapshot> coursesList = snapshot.data!.docs;
 
+          // If no courses are available, show a message
           if (coursesList.isEmpty) {
             return const Center(
               child: Text(
                 'No Courses found',
                 style: TextStyle(
-                  color: Colors.red,
+                  color: Colors.red, // Red text for no courses
                 ),
               ),
             );
           }
 
+          // Build a list of course widgets
           return ListView.builder(
-            shrinkWrap: true,
+            shrinkWrap: true, // Shrink wrap for embedding in scrollable widget
             physics: const NeverScrollableScrollPhysics(),
             itemCount: coursesList.length,
             itemBuilder: (context, index) {
@@ -176,10 +194,12 @@ class CourseList extends StatelessWidget {
               Map<String, dynamic> data =
                   document.data() as Map<String, dynamic>;
 
+              // Extract course details or default values if missing
               String courseName = data['courseName'] ?? 'No Name';
               double courseFees = data['fees'] ?? 'No Email';
               String courseDuration = data['courseDuration'] ?? 'No Data';
 
+              // Display individual course item
               return Course(
                 courseName: courseName,
                 fees: courseFees,
@@ -188,12 +208,15 @@ class CourseList extends StatelessWidget {
             },
           );
         } else if (snapshot.hasError) {
-          log('Error: ${snapshot.error.toString()}');
+          log('Error: ${snapshot.error.toString()}'); // Log errors
           return const Center(
-            child: Text('Error loading students',
-                style: TextStyle(color: Colors.red)),
+            child: Text(
+              'Error loading students',
+              style: TextStyle(color: Colors.red), // Error message styling
+            ),
           );
         } else {
+          // Show loading indicator while fetching data
           return const Center(
             child: CircularProgressIndicator(),
           );
@@ -203,43 +226,53 @@ class CourseList extends StatelessWidget {
   }
 }
 
+// Stateless widget for displaying individual course details
 class Course extends StatelessWidget {
   String courseDuration;
   String courseName;
   double fees;
-  Course(
-      {super.key,
-      required this.courseName,
-      required this.fees,
-      required this.courseDuration});
+
+  // Constructor for the Course widget
+  Course({
+    super.key,
+    required this.courseName,
+    required this.fees,
+    required this.courseDuration,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Container to style the course card
         Container(
           width: MediaQuery.of(context).size.width,
-          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+          margin:
+              const EdgeInsets.symmetric(horizontal: 5.0), // Horizontal margins
           decoration: BoxDecoration(
             border: Border.all(
-              color: Colors.blue,
+              color: Colors.blue, // Blue border
             ),
             borderRadius: const BorderRadius.all(
               Radius.circular(18),
             ),
           ),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
+            padding: const EdgeInsets.symmetric(
+                horizontal: 10), // Padding inside the card
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                margin(width: 0, height: 20),
+                margin(width: 0, height: 20), // Spacing
+                // Course image with rounded corners
                 ClipRRect(
                   borderRadius: BorderRadius.circular(18),
-                  child: Image.asset('assets/courses/JS.png'),
+                  child:
+                      Image.asset('assets/courses/JS.png'), // Placeholder image
                 ),
-                margin(width: 0, height: 10),
+                margin(width: 0, height: 10), // Spacing
+                // Display course name and duration
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -248,22 +281,25 @@ class Course extends StatelessWidget {
                         courseName,
                         textAlign: TextAlign.start,
                         style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 19,
-                            fontWeight: FontWeight.bold),
+                          color: Colors.white,
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     Text(
                       'Duration - $courseDuration months',
                       textAlign: TextAlign.start,
                       style: const TextStyle(
-                          color: Colors.white54,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal),
+                        color: Colors.white54,
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                      ),
                     ),
                   ],
                 ),
-                margin(width: 0, height: 20),
+                margin(width: 0, height: 20), // Spacing
+                // Display course fees and enroll button
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10),
                   child: Row(
@@ -277,30 +313,34 @@ class Course extends StatelessWidget {
                         ),
                       ),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {}, // Placeholder for enroll action
                         style: ButtonStyle(
                           backgroundColor: WidgetStateProperty.all(
                               Colors.blue), // Blue background
                           shape: WidgetStateProperty.all(
                             RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
+                              borderRadius:
+                                  BorderRadius.circular(12), // Rounded corners
                             ),
                           ),
                         ),
                         child: const Text(
                           "Enroll Now",
-                          style: TextStyle(color: Colors.white),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
-                margin(width: 0, height: 20),
+                margin(width: 0, height: 20), // Spacing
               ],
             ),
           ),
         ),
-        margin(width: 0, height: 20)
+        margin(width: 0, height: 15), // Spacing between course cards
       ],
     );
   }
